b122b2bc27a92f54882c7defa3fdc074ffdefd1e
Otto kai <121492489+yhlh9982@users.noreply.github.com>
2024-05-03 00:19:37 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -35,6 +35,7 @@ echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
 if [[ $WRT_URL == *"lede"* ]]; then
 	echo "CONFIG_PACKAGE_luci-app-ssr-plus=n" >> ./.config
 	echo "CONFIG_PACKAGE_luci-app-openclash=n" >> ./.config
+        echo "CONFIG_PACKAGE_luci-app-smartdns=y" >> ./.config
 elif [[ $WRT_URL == *"immortalwrt"* ]]; then
 	echo "CONFIG_PACKAGE_luci=y" >> ./.config
 	echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
