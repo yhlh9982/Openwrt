da9de66857fb963d664113d8f735eb34c9859fc8
Otto kai <121492489+yhlh9982@users.noreply.github.com>
2024-05-05 00:26:00 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -68,9 +68,9 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-
 		mv -f $REPO_NAME $PKG_NAME
 	fi
 }
-#UPDATE_PACKAGE "luci-app-smartdns" "pymumu/luci-app-smartdns" "lede"
+UPDATE_PACKAGE "luci-app-smartdns" "pymumu/luci-app-smartdns" "lede"
 #UPDATE_PACKAGE "luci-app-smartdns" "pymumu/luci-app-smartdns" "master"
-UPDATE_PACKAGE "smartdns" "pymumu/luci-app-smartdns" "$([[ $WRT_URL == *"lede"* ]] && echo "lede" || echo "master")"
+#UPDATE_PACKAGE "smartdns" "pymumu/luci-app-smartdns" "$([[ $WRT_URL == *"lede"* ]] && echo "lede" || echo "master")"
 
 UPDATE_PACKAGE "design" "gngpp/luci-theme-design" "$([[ $WRT_URL == *"lede"* ]] && echo "main" || echo "js")"
 UPDATE_PACKAGE "design-config" "gngpp/luci-app-design-config" "master"
